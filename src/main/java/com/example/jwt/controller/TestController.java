de2ed328f4fe32bec9e50c87aad86208525c388d
manjula <manjulakvkaki86@gmail.com>
2026-02-04 20:04:54 +0530

    Remove accidental submodule and restore project structure

---
+++ src/main/java/com/example/jwt/controller/TestController.java
@@ -0,0 +1,15 @@
+package com.example.jwt.controller;
+
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "JWT Protected API Accessed!";
+    }
+}
